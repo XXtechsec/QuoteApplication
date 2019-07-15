75155625aa023a6ed8e8e33c7097af6ff2c4cb79
Anthony Lazar <tony@AnthonyMac.local>
2019-07-15 11:44:16 -0400

    make sure

--- Quotes/models.py
+++ Quotes/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 
 class Service(models.Model):
-    ServiceType = models.CharField(max_length = 100)
-    Type = models.CharField(max_length = 100)
-    Quality = models.CharField(max_length = 100)
     SKU = models.CharField(db_column='VendorPartNumber', max_length = 20)
     Description = models.CharField(db_column='Description', max_length = 200)
     Price = models.FloatField(db_column='Cost')
