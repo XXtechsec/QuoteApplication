c4055df86f040fc79efb0833aa1ac0b0433faacb
Anthony Lazar <tony@AnthonyMac.local>
2019-07-15 11:40:19 -0400

    check if getting data works

--- Quotes/models.py
+++ Quotes/models.py
@@ -4,11 +4,13 @@ class Service(models.Model):
     ServiceType = models.CharField(max_length = 100)
     Type = models.CharField(max_length = 100)
     Quality = models.CharField(max_length = 100)
-    SKU = models.CharField(max_length = 20)
-    Description = models.CharField(max_length = 200)
-    Price = models.FloatField()
-    Qty = models.IntegerField()
+    SKU = models.CharField(db_column='VendorPartNumber', max_length = 20)
+    Description = models.CharField(db_column='Description', max_length = 200)
+    Price = models.FloatField(db_column='Cost')
+    Qty = models.IntegerField(default=1)
 
+    class Meta:
+        db_table = 'Products_CommerxCatalog_Products'
     def __str__(self):
         return self.Description
 
