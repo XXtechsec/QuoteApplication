a03af7fd1d8fd480cb5cf03f820a1573da8c5c30
Anthony Lazar <tony@AnthonyMac.local>
2019-07-18 16:36:41 -0400

    sys path

--- QuoteApplication/wsgi.py
+++ QuoteApplication/wsgi.py
@@ -8,7 +8,10 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
+path = '/var/www/QuoteApplication'
+if path is not in sys.path:
+    sys.path.append(path)
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'QuoteApplication.settings')
