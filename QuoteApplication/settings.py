ca4346ca70bff2849e746a58cd1c2f62773720c0
Anthony Lazar <tony@AnthonyMac.local>
2019-07-19 18:00:09 -0400

    ?

--- QuoteApplication/settings.py
+++ QuoteApplication/settings.py
@@ -62,7 +62,6 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR, 'Templates')],
-        'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
