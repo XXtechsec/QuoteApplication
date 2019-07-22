a6b8de8f9e76767b56a4f46e858e7c9bee64ba33
Anthony Lazar <tony@AnthonyMac.local>
2019-07-21 21:01:20 -0400

    fix

--- Quotes/render.py
+++ Quotes/render.py
@@ -2,12 +2,21 @@ from io import BytesIO
 from django.http import HttpResponse
 from django.template.loader import get_template
 import xhtml2pdf.pisa as pisa
-
+from django.template import RequestContext
+from django.conf import settings
+import ho.pisa as pisa
+import cStringIO as StringIO
+import cgi
+import os
 
 class Render:
+    def fetch_resources(uri, rel):
+        if uri.startswith(sUrl):
+            sUrl = settings.STATIC_URL
+            sRoot = settings.STATIC_ROOT
+            path = os.path.join(sRoot, uri.replace(sUrl, ""))
+            return path
 
-    #for converting it to PDF nothing much to change
-    @staticmethod
     def render(path: str, params: dict):
         template = get_template(path)
         html = template.render(params)
@@ -17,9 +26,3 @@ class Render:
             return HttpResponse(response.getvalue(), content_type='application/pdf')
         else:
             return HttpResponse("Error Rendering PDF", status=400)
-    def fetch_resources(uri, rel):
-        if uri.startswith(sUrl):
-            sUrl = settings.STATIC_URL
-            sRoot = settings.STATIC_ROOT
-            path = os.path.join(sRoot, uri.replace(sUrl, ""))
-            return path
