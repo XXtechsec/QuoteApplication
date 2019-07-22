3f33f961b76dd1562648b8682182df770accf1a0
Anthony Lazar <tony@AnthonyMac.local>
2019-07-21 20:33:52 -0400

    get pdf to work

--- Quotes/render.py
+++ Quotes/render.py
@@ -12,8 +12,14 @@ class Render:
         template = get_template(path)
         html = template.render(params)
         response = BytesIO()
-        pdf = pisa.pisaDocument(BytesIO(html.encode("UTF-8")), response)
+        pdf = pisa.pisaDocument(BytesIO(html.encode("UTF-8")), response, link_callback=fetch_resources)
         if not pdf.err:
             return HttpResponse(response.getvalue(), content_type='application/pdf')
         else:
             return HttpResponse("Error Rendering PDF", status=400)
+    def fetch_resources(uri, rel):
+        if uri.startswith(sUrl):
+            sUrl = settings.STATIC_URL
+            sRoot = settings.STATIC_ROOT
+            path = os.path.join(sRoot, uri.replace(sUrl, ""))
+            return path
